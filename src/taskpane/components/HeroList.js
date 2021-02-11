8b29bd762ffda58dcf0bfbf18bc737afa2a3b2c1
Martin Žilák <martin.zilak@artinsolutions.com>
2021-02-11 19:47:36 +0100

    Rewrote demo components to functional style.

--- src/taskpane/components/HeroList.js
+++ src/taskpane/components/HeroList.js
@@ -1,21 +1,20 @@
 import * as React from "react";
 
-export default class HeroList extends React.Component {
-  render() {
-    const { children, items, message } = this.props;
-
+const HeroList = ({ children, items, message }) => {
     const listItems = items.map((item, index) => (
-      <li className="ms-ListItem" key={index}>
-        <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
-        <span className="ms-font-m ms-fontColor-neutralPrimary">{item.primaryText}</span>
-      </li>
+        <li className="ms-ListItem" key={index}>
+            <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
+            <span className="ms-font-m ms-fontColor-neutralPrimary">{item.primaryText}</span>
+        </li>
     ));
+
     return (
-      <main className="ms-welcome__main">
-        <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
-        <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">{listItems}</ul>
-        {children}
-      </main>
+        <main className="ms-welcome__main">
+            <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
+            <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">{listItems}</ul>
+            {children}
+        </main>
     );
-  }
-}
+};
+
+export default HeroList;
